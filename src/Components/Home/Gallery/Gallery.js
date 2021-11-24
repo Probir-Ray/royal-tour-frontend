ca6a6af5c16291c3b808f9939c2938a6f771885d
Probir-Ray <somironroy.need@gmail.com>
2021-11-24 22:20:38 +0600

    update

--- src/Components/Home/Gallery/Gallery.js
+++ src/Components/Home/Gallery/Gallery.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Gallery = () => {
     return (
         <div className="container-fluid my-5 p-0 py-5">
-            <h3 className="text-center my-4 text-uppercase">Tour Album</h3>
+            <h3 className="text-center my-4 text-uppercase">Gallery</h3>
             <div className="row m-0 p-0">
                 <div className="col-md-4 p-0">
                     <img className="card-img-top img-fluid" src="https://i.ibb.co/G39GYy8/AQUA-DOME.jpg" alt="" />
